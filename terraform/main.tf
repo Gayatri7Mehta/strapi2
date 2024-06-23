aa11222844362603a0958076b9bae6214dd03ba7
Gayatri <gayatri29mehta@gmail.com>
2024-06-23 21:24:17 +0530

    workflows changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,10 +55,10 @@ resource "aws_security_group" "strapi_sg" {
 }
 
 resource "aws_instance" "strapi" {
-  ami             = "ami-0f58b397bc5c1f2e8" 
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public.id
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]  # Use vpc_security_group_ids instead of security_groups
+  ami                   = "ami-0f58b397bc5c1f2e8"
+  instance_type         = "t2.micro"
+  subnet_id             = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id] 
 
   user_data = <<-EOF
                 #!/bin/bash
